123ba5aeb9b85e2363fae9c7581a672d719acf72
Jay89-tech <59422784+Jay89-tech@users.noreply.github.com>
2025-11-13 09:23:16 +0200

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SkillsManagement.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
